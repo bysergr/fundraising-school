a41016df7ca6c2acc8859b151d352ec951bdbe5f
Diego Berrocal <3291619+CestDiego@users.noreply.github.com>
2024-07-26 19:32:24 -0400

    Connect search to OndeVamosAPIv0

--- app/(product)/product/search_list/page.tsx
+++ app/(product)/product/search_list/page.tsx
@@ -1,17 +1,16 @@
-import DownloadVC from '@/components/vc_list/download-vc';
-import FilterBar from '@/components/vc_list/filter-bar';
-import SearchInput from '@/components/vc_list/search-input';
-import TableVC from '@/components/vc_list/table-vc';
 import TitleSection from '@/components/vc_list/title-section';
 import by_onde from '@/public/images/ctw/by_onde.svg';
-
+import dynamic from 'next/dynamic';
 import { Session, getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/auth';
-
 import FundModal from '@/components/vc_list/fund_modal';
 import Image from 'next/image';
-import { SearchEvents } from '@/components/search_list/searchEvents';
 import Link from 'next/link';
+
+const DynamicSearchEvents = dynamic(() => import('@/components/search_list/searchEvents'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
 export default async function Page() {
   const data: Session = (await getServerSession(authOptions)) as Session;
@@ -19,16 +18,16 @@ export default async function Page() {
   return (
     <>
       <FundModal />
-      <TitleSection nameSection="Agenda" description="Browse all events and create your calendar" >
+      <TitleSection nameSection="Agenda" description="Browse all events and create your calendar">
         <div className="block lg:hidden">
-          <Link href="https://onde-vamos.com/" target='_blank' className="bg-[red]">
-            <Image src={by_onde} alt="by Onde" className="w-16 ml-1 mt-2" />
+          <Link href="https://onde-vamos.com/" target="_blank" className="bg-[red]">
+            <Image src={by_onde} alt="by Onde" className="ml-1 mt-2 w-16" />
           </Link>
         </div>
       </TitleSection>
       <div className="size-full p-0">
         <div className="h-screen w-full">
-          <SearchEvents serverUrl="" />
+          <DynamicSearchEvents serverUrl="" />
         </div>
       </div>
     </>
---
+++ utils/onde-vamos.ts
@@ -0,0 +1,56 @@
+import Typesense from 'typesense';
+import type { ConfigurationOptions } from 'typesense/lib/Typesense/Configuration';
+import TypesenseInstantSearchAdapterExport, {
+    type TypesenseInstantsearchAdapterOptions,
+} from 'typesense-instantsearch-adapter';
+// There is an issue with this being compatible with vite
+// https://github.com/typesense/typesense-instantsearch-adapter/issues/199
+export const TypesenseInstantSearchAdapter =
+    TypesenseInstantSearchAdapterExport.default ?? TypesenseInstantSearchAdapterExport;
+
+const ONDE_VAMOS_API_KEY = 'lasnf791hp3;iuTotallySafebnposcb-v97xoiusf';
+const ONDE_VAMOS_HOST =
+    process.env.NODE_ENV === 'production' ? 'api.onde-vamos.com' : 'api.dev.onde-vamos.com';
+
+if (
+    !ONDE_VAMOS_API_KEY ||
+    ONDE_VAMOS_API_KEY === '' ||
+    !ONDE_VAMOS_HOST ||
+    ONDE_VAMOS_HOST === ''
+) {
+    throw new Error('Missing required environment variables for Typesense');
+}
+
+export const OndeSearchConfig = {
+    apiKey: ONDE_VAMOS_API_KEY,
+    nodes: [
+        {
+            host: ONDE_VAMOS_HOST,
+            port: 443,
+            protocol: 'https',
+        },
+    ],
+} satisfies ConfigurationOptions;
+
+const TypesenseAdapterOptions = {
+    server: OndeSearchConfig,
+    // The following parameters are directly passed to Onde's search API endpoint.
+    // So you can pass any parameters supported by the search endpoint below.
+    // queryBy is required.
+    additionalSearchParameters: {
+        query_by: 'title,description',
+        sort_by: '_text_match:desc',
+        exclude_fields: 'embedding',
+        prefix: false,
+    },
+    // additionalSearchParameters: {
+    //     query_by: 'embedding,title,description',
+    //     vector_query: 'embedding:([], alpha: 0.8)',
+    //     sort_by: '_text_match:desc',
+    //     exclude_fields: 'embedding,description',
+    //     prefix: false,
+    // },
+} satisfies TypesenseInstantsearchAdapterOptions;
+
+export const client = new Typesense.Client(OndeSearchConfig);
+export const getTypesenseAdapter = () => new TypesenseInstantSearchAdapter(TypesenseAdapterOptions);
