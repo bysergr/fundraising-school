8cd30e5f491570ed7318573dc020fc66c9225f8d
Diego Berrocal <3291619+CestDiego@users.noreply.github.com>
2024-07-30 20:21:20 -0400

    Fix lint

--- utils/onde-vamos/common.ts
+++ utils/onde-vamos/common.ts
@@ -4,40 +4,40 @@
  * @see index.ts
  */
 export const ONDE_VAMOS_HOST =
-    process.env.NODE_ENV === 'production' ? 'api.onde-vamos.com' : 'api.diego.dev.onde-vamos.com';
+  process.env.NODE_ENV === 'production' ? 'api.onde-vamos.com' : 'api.diego.dev.onde-vamos.com';
 export const ONDE_VAMOS_API_KEY = 'lasnf791hp3;iuTotallySafebnposcb-v97xoiusf';
 export const ONDE_VAMOS_API_BASE_URL = `https://${ONDE_VAMOS_HOST}/colombiatechweek`;
 
 export type HostProps = {
-    instagram: string[];
-    linkedin: string[];
-    name: string[];
-    twitter: string[];
+  instagram: string[];
+  linkedin: string[];
+  name: string[];
+  twitter: string[];
 };
 
 export type TechWeekHit = {
-    title: string;
-    description: string;
-    categories: string[];
-    date_start: string;
-    start_time: string;
-    end_time: string;
-    full_address: string;
-    url: string;
-    host?: HostProps[];
-    neighborhood?: string;
-    relevanceExplanation?: string;
-    relevanceScore: number;
-    remove?: boolean;
+  title: string;
+  description: string;
+  categories: string[];
+  date_start: string;
+  start_time: string;
+  end_time: string;
+  full_address: string;
+  url: string;
+  host?: HostProps[];
+  neighborhood?: string;
+  relevanceExplanation?: string;
+  relevanceScore: number;
+  remove?: boolean;
 };
 
 // This type is a type returned from our search + some customization based on the user context.
 // Like isAddedToCalendar that tracks whether the event is added to the user's calendar.
 export type TechWeekEvent = TechWeekHit & {
-    isAddedToCalendar?: boolean;
+  isAddedToCalendar?: boolean;
 };
 
 export type ScheduleProps = {
-    date: string;
-    events: TechWeekEvent[];
+  date: string;
+  events: TechWeekEvent[];
 };
